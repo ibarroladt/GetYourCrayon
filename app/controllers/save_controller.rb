5c418884b0878bbf812985bb34de6d9d07bc8e0d
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 12:28:07 -0500

    adds ability to save drawings from the chrome extension

--- app/controllers/save_controller.rb
+++ app/controllers/save_controller.rb
@@ -1,5 +1,11 @@
 class SaveController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
   def save
-    "Hello"
+    p 'X' * 100
+    p
+    p 'X' * 100
+    @website = Website.find_or_create_by_url(params[:url])
+    @drawing = @website.drawings.create(content: params[:json_string])
+    "Success"
   end
 end

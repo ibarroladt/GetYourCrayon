f8158051d6b6246c1a07e25d5583c57f9119f706
Apprentice <apprentice@dbc-chicago-001.local>
2013-09-28 16:19:31 -0500

    Modified retrieve action to send tags

--- app/controllers/save_controller.rb
+++ app/controllers/save_controller.rb
@@ -27,12 +27,16 @@ class SaveController < ApplicationController
     else
       #what to do on initial page load - grab the latest drawing, also pass max id
       website = Website.find_by_url(params[:url])
+      tag_objects = website.tags.order("created_at DESC").limit(5)
+      tags_html_string = make_html(tag_objects).join("")
+      # send tag_names to helper method that creates links for each tag
+
       if website.nil?
         render :text => "website not found"
       else
         json_string = website.drawings.last.content
         max_index = website.drawings.length - 1
-        render :json => {"json_string" => json_string, "max_index" => max_index}.to_json
+        render :json => {"json_string" => json_string, "max_index" => max_index, "tags_html_string" => tags_html_string}.to_json
         #render :text => json_string
       end
     end

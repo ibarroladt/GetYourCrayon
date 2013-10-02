1092a646b3d95a9ce994d5cf0c1cdb76cbb86fcc
nlprater <prater.nick@gmail.com>
2013-10-01 20:14:15 -0500

    added colors to the tag cloud

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,11 +1,14 @@
 var cloudify = function() {
     var tagsArray = $('#tagCloud a');
     var highCount = $('#tagCloud').attr('data-maxcount');
+    var colorArray = ["#0B88BE" ,"#EE094C", "#FC4A04", "#FFA205", "#35AB95", "#2F9885"];
 
     for (var i=0; i<tagsArray.length; i++) {
+    	var selectedColor = colorArray[Math.floor(Math.random()*6)];
         var tagLink = $(tagsArray[i]);
         var fontRatio = highCount / tagLink.data('count');
         tagLink.css("font-size", (5 / fontRatio) + "em"); 
+        tagLink.css("color", selectedColor);
     }
 }
 
